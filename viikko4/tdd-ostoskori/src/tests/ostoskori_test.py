85053de46daa55a58da4f5f1c25dd03eabb1a511
PaulusParssinen <paulus.parssinen@gmail.com>
2022-03-24 23:43:05 +0200

    tdd-ostoskori: Test for adding two products

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -16,3 +16,11 @@ class TestOstoskori(unittest.TestCase):
         
         self.assertEqual(self.kori.hinta(), 3)
         self.assertEqual(self.kori.tavaroita_korissa(), 1)
+        
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
