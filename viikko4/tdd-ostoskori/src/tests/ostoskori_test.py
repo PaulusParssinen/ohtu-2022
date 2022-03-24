cd20c88f937bb88465f6fd134ad786066013ff17
PaulusParssinen <paulus.parssinen@gmail.com>
2022-03-25 00:09:17 +0200

    tdd-ostoskori: Test for removing the only product in cart. [Step 14]

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -82,3 +82,12 @@ class TestOstoskori(unittest.TestCase):
         self.kori.poista_tuote(maito)
         
         self.assertEqual(len(self.kori.ostokset()), 1)
+    
+    def tuotteen_lisaamisen_ja_poistamisen_jalkeen_tyhja_kori(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.poista_tuote(maito)
+        
+        self.assertEqual(len(self.kori.hinta()), 0)
+        self.assertEqual(len(self.kori.ostokset()), 0)
+        self.assertEqual(len(self.kori.tavaroita_korissa()), 0)
