bace433e2092945893591d4ac5a87d95571a7669
PaulusParssinen <paulus.parssinen@gmail.com>
2022-03-24 23:45:20 +0200

    tdd-ostoskori: Test for calculating total price of two different products in the cart

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -25,3 +25,12 @@ class TestOstoskori(unittest.TestCase):
         
         self.assertEqual(self.kori.hinta(), 6)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+        
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        maito = Tuote("Kahvi", 5)
+        self.kori.lisaa_tuote(maito)
+        
+        self.assertEqual(self.kori.hinta(), 8)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
